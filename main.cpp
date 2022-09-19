40c1428ebb5d47c4167cab2ea66bb5520170c27d
martinigergo <martinigergo@gmail.com>
2022-09-19 19:32:17 +0200

    hibak megjelolese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //csillag rossz helyen van, []-ben eliras
+    std::cout << '1-100 ertekek duplazasa' // hianyos a vege,lezaras
+    for (int i = 0;) // int utan a zarojelben levo resz hibas
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //[]-ben levo resz hibas
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //kozepso i resz hianyos
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hianyos a vege,lezaras
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //hianyos az atlag utani resz
+    for (int i = 0; i < N_ELEMENTS, i++) 
     {
-        atlag += b[i]
+        atlag += b[i] //; hiany
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;

9374c73bb7d38e2003a4fbf004d36acc0a4e5bb5
martinigergo <martinigergo@gmail.com>
2022-09-19 19:35:16 +0200

    javitott

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-#include <iostream>
-
-constexpr int N_ELEMENTS = 100;
-
-int main()
-{
-    int *b = new int[NELEMENTS]; //csillag rossz helyen van, []-ben eliras
-    std::cout << '1-100 ertekek duplazasa' // hianyos a vege,lezaras
-    for (int i = 0;) // int utan a zarojelben levo resz hibas
+int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+        for (int i = 1;i<N_ELEMENTS+1;i++)
+        {
+            b[i-1] = i * 2;
+        }
+    for (int i = 0; i<=N_ELEMENTS-1; i++)
     {
-        b[i] = i * 2; //[]-ben levo resz hibas
+        std::cout << "Ertek:" << b[i] << std::endl;
     }
-    for (int i = 0; i; i++) //kozepso i resz hianyos
-    {
-        std::cout << "Ertek:" // hianyos a vege,lezaras
-    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //hianyos az atlag utani resz
-    for (int i = 0; i < N_ELEMENTS, i++) 
+    int atlag=0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //; hiany
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
-}
